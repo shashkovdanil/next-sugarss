6946c26354bbcd6f71df287417d40736a11191a6
shashkovdanil <shashkovdanil@gmail.com>
2019-03-31 22:39:31 +0500

    plugin

---
+++ sss-loader-config.js
@@ -0,0 +1,129 @@
+const ExtractCssChunks = require('extract-css-chunks-webpack-plugin')
+const findUp = require('find-up')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+
+const fileExtensions = new Set()
+let extractCssInitialized = false
+
+module.exports = (
+  config,
+  {
+    extensions = [],
+    cssModules = false,
+    cssLoaderOptions = {},
+    dev,
+    isServer,
+    postcssLoaderOptions = {},
+    loaders = []
+  }
+) => {
+  // We have to keep a list of extensions for the splitchunk config
+  for (let extension of extensions) {
+    fileExtensions.add(extension)
+  }
+
+  if (!isServer) {
+    config.optimization.splitChunks.cacheGroups.styles = {
+      name: 'styles',
+      test: new RegExp(`\\.+(${[...fileExtensions].join('|')})$`), // eslint-disable-line
+      chunks: 'all',
+      enforce: true
+    }
+  }
+
+  if (!isServer && !extractCssInitialized) {
+    config.plugins.push(
+      new ExtractCssChunks({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: dev
+          ? 'static/chunks/[name].css'
+          : 'static/chunks/[name].[contenthash:8].css',
+        chunkFilename: dev
+          ? 'static/chunks/[name].chunk.css'
+          : 'static/chunks/[name].[contenthash:8].chunk.css',
+        hot: dev,
+        reloadAll: dev
+      })
+    )
+    extractCssInitialized = true
+  }
+
+  if (!dev) {
+    if (!Array.isArray(config.optimization.minimizer)) {
+      config.optimization.minimizer = []
+    }
+
+    config.optimization.minimizer.push(
+      new OptimizeCssAssetsWebpackPlugin({
+        cssProcessorOptions: {
+          discardComments: { removeAll: true }
+        }
+      })
+    )
+  }
+
+  let postcssConfigPath = findUp.sync('postcss.config.js', {
+    cwd: config.context
+  })
+  let postcssLoader
+
+  if (postcssConfigPath) {
+    // Copy the postcss-loader config options first.
+    let postcssOptionsConfig = Object.assign({}, postcssLoaderOptions.config, {
+      path: postcssConfigPath
+    })
+
+    postcssLoader = {
+      loader: 'postcss-loader',
+      options: Object.assign({}, postcssLoaderOptions, {
+        parser: 'sugarss',
+        config: Object.assign(postcssOptionsConfig, {
+          ctx: {
+            'postcss-preset-env': {
+              browsers: 'defaults'
+            },
+            'postcss-easy-import': {
+              extensions: ['.sss']
+            },
+            'postcss-nested': {},
+            precss: {},
+            cssnano: {}
+          }
+        })
+      })
+    }
+  }
+
+  let cssLoader = {
+    loader: 'css-loader',
+    options: Object.assign(
+      {},
+      {
+        modules: cssModules,
+        sourceMap: dev,
+        importLoaders: loaders.length + (postcssLoader ? 1 : 0),
+        exportOnlyLocals: isServer
+      },
+      cssLoaderOptions
+    )
+  }
+
+  // When not using css modules we don't transpile on the server
+  if (isServer && !cssLoader.options.modules) {
+    return ['ignore-loader']
+  }
+
+  // When on the server and using css modules we transpile the css
+  if (isServer && cssLoader.options.modules) {
+    return [cssLoader, postcssLoader, ...loaders].filter(Boolean)
+  }
+
+  return [
+    dev && ExtractCssChunks.hotLoader,
+    !isServer && ExtractCssChunks.loader,
+    cssLoader,
+    postcssLoader,
+    ...loaders
+  ].filter(Boolean)
+}
